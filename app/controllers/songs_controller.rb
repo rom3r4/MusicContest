00590544de5b851fac602f864202ae55c35965a9
julian <julian.romera@gmail.com>
2019-11-19 22:38:20 +0100

    Disable rubocop MixinUsage for bin/setup and bin/update

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -37,6 +37,8 @@ class SongsController < ApplicationController
   end
 
   def song_params
-    params.require(:song).permit(:spotify_id, :spotify_url, :spotify_title, :spotify_artist, :spotify_length, :spotify_album, :spotify_cover_id, :contest_id, :submitby_user_id)
+    params.require(:song).permit(:spotify_id, :spotify_url, :spotify_title,
+                                 :spotify_artist, :spotify_length, :spotify_album,
+                                 :spotify_cover_id, :contest_id, :submitby_user_id)
   end
 end
--- lib/exception_handler.rb
+++ lib/exception_handler.rb
@@ -29,23 +29,23 @@ module ExceptionHandler
   private
 
   # JSON response with message; Status code 404 - Not found
-  def record_not_found(e)
-    render json: {error: e.message}, status: :not_found
+  def record_not_found(raised_exception)
+    render json: {error: raised_exception.message}, status: :not_found
   end
 
   # JSON response with message; Status code 401 - Unauthorized
-  def access_denied(e)
-    render json: {error: e.message}, status: :forbidden
+  def access_denied(raised_exception)
+    render json: {error: raised_exception.message}, status: :forbidden
   end
 
   # JSON response with message; Status code 501 - Server error
-  def server_error(e)
-    render json: {error: e.message}, status: 501
+  def server_error(raised_exception)
+    render json: {error: raised_exception.message}, status: 501
   end
 
   # JSON response with message; Status code 422 - unprocessable entity
-  def four_two_two(e)
-    render json: {error: e.message}, status: :unprocessable_entity
+  def four_two_two(raised_exception)
+    render json: {error: raised_exception.message}, status: :unprocessable_entity
   end
 
   # JSON response with message; Status code 404 - Not available
@@ -59,7 +59,7 @@ module ExceptionHandler
   end
 
   # JSON response with message; Status code 401 - Unauthorized
-  def generic_unauthorized(e)
-    render json: {error: e.message}, status: :unauthorized
+  def generic_unauthorized(raised_exception)
+    render json: {error: raised_exception.message}, status: :unauthorized
   end
 end
