4d09a9964a84b459f2a9be9867ff3ac80bc2c04f
julian <julian.romera@gmail.com>
2019-11-19 02:22:18 +0100

    Update Routes default returning format. Correct remaining specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :song_covers
-  resources :songs
-  resources :participants
-  resources :music_contests
-  resources :contest_song_participant_votes
+  defaults format: :json do
+    resources :song_covers
+    resources :songs
+    resources :participants
+    resources :music_contests
+    resources :contest_song_participant_votes
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
