92f784d6a4a3f4cc4e04d8c5fddc75ba599b2c4d
julian <julian.romera@gmail.com>
2019-11-20 02:03:08 +0100

    Test Api with Postman. Dump seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,13 @@ Participant.create!([
                       {name: "name1", surname: "surname1"},
                       {name: "name2", surname: "surname2"}
                     ])
+
+SongCover.create!([
+                    {file_path: "/files/file1.jpeg", file_type: "jpeg", file_url: "https://s3.amazonws.com/"}
+                  ])
 Song.create!([
-               {spotify_id: "id1", spotify_url: "url1", spotify_title: "title1", spotify_artist: "artist1", spotify_length: 40, spotify_album: "album1", spotify_cover_id: nil, contest_id: 1, submitby_user_id: 1},
-               {spotify_id: "id2", spotify_url: "url2", spotify_title: "title2", spotify_artist: "artist2", spotify_length: 40, spotify_album: "album2", spotify_cover_id: nil, contest_id: 1, submitby_user_id: 2}
+               {spotify_id: "id1", spotify_url: "url1", spotify_title: "title1", spotify_artist: "artist1", spotify_length: 40, spotify_album: "album1", spotify_cover_id: 1, contest_id: 1, submitby_user_id: 1},
+               {spotify_id: "id2", spotify_url: "url2", spotify_title: "title2", spotify_artist: "artist2", spotify_length: 40, spotify_album: "album2", spotify_cover_id: 1, contest_id: 1, submitby_user_id: 2}
              ])
 ContestSongParticipantVote.create!([
                                      {participant_id: 1, song_id: 1},
